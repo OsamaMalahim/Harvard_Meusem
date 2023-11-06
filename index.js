9359d06065a38149fec4d2ef870ed997b4a4ccac
OsamaMalahim <engosm2009@gmail.com>
2023-11-06 11:20:47 +0300

    version 0 loading images ... ok

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// to bypass localissuer certificate failure
+//export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 //All requests to the API begin with:
 const API_URL = "https://api.harvardartmuseums.org";
@@ -15,12 +19,22 @@ const API_KEY = "fadfdbd7-8c2a-483b-ad1d-26134e78590f";
 
 
 app.get("/", async(req,res)=>{
-    const config = {apikey: API_KEY, size: 1};
-    const result = await axios.get(API_URL_IMAGE,config);
+    try {
+        
+    const result = await axios.get(API_URL_IMAGE,{params: {
+        apikey: API_KEY, sort: "random",
+        size: 1
+    }});
+
     const ResultJSObject = result.data;
+    console.log("Result JSON Object: >>>"+JSON.stringify(ResultJSObject));
     console.log("base Image>>>" + ResultJSObject.records[0].baseimageurl);
     
-    res.render("index.ejs", {data: ResultJSObject.records[0].imageid});
+    res.render("index.ejs", {data: ResultJSObject.records[0]});
+
+    } catch (error) {
+        console.log("API error message:"+ error.message);
+    }
 });
 
 app.listen(port,(req,res)=>{
